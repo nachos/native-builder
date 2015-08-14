3ade171f288ab6b0b07db1a65363fbb036c91fb7
Amir Schlezinger <amir.shrir@gmail.com>
2015-08-14 12:41:11 +0300

    Covered all test cases for native-builder

--- test/main.spec.js
+++ test/main.spec.js
@@ -20,7 +20,44 @@ describe('native-builder', function () {
     });
   });
 
-  describe('Running atom shell', function () {
+  describe('Running atom shell win32', function () {
+    before(function () {
+      var deferred = Q.defer();
+      var whichNativeStub = sinon.stub().returns(deferred.promise);
+
+      deferred.resolve({ asVersion: '0.30.1' });
+      mockery.registerMock('which-native-nodish', whichNativeStub);
+      mockery.enable({
+        useCleanCache: true,
+        warnOnReplace: false,
+        warnOnUnregistered: false
+      });
+
+      this.originalPlatform = process.platform;
+      Object.defineProperty(process, 'platform', {
+        value: 'win32'
+      });
+    });
+
+    after(function () {
+      mockery.deregisterMock('which-native-nodish');
+      mockery.disable();
+      Object.defineProperty(process, 'platform', {
+        value: this.originalPlatform
+      });
+    });
+
+    it('should work', function () {
+      var nativeBuilder = require('../lib');
+      var builder = path.resolve(__dirname, '..', 'node_modules', '.bin', 'pangyp');
+      var electronSetup = 'SET USERPROFILE=%USERPROFILE%\\.electron-gyp&& ';
+      var distUrl = '  --dist-url=https://atom.io/download/atom-shell';
+
+      return assert.eventually.equal(nativeBuilder.resolve(), electronSetup.concat(builder, ' rebuild --target=0.30.1', distUrl));
+    });
+  });
+
+  describe('Running atom shell otherOS', function () {
     before(function () {
       var deferred = Q.defer();
       var whichNativeStub = sinon.stub().returns(deferred.promise);
@@ -37,13 +74,22 @@ describe('native-builder', function () {
     after(function () {
       mockery.deregisterMock('which-native-nodish');
       mockery.disable();
+      Object.defineProperty(process, 'platform', {
+        value: this.originalPlatform
+      });
     });
 
     it('should work', function () {
       var nativeBuilder = require('../lib');
       var builder = path.resolve(__dirname, '..', 'node_modules', '.bin', 'pangyp');
-      var electronSetup = 'SET USERPROFILE=%USERPROFILE%\\.electron-gyp&& ';
+      var electronSetup = 'HOME=~/.electron-gyp ';
       var distUrl = '  --dist-url=https://atom.io/download/atom-shell';
+
+      // Can't happen before the creation of native builder, since it ruins the child-process require
+      this.originalPlatform = process.platform;
+      Object.defineProperty(process, 'platform', {
+        value: 'JustSomeOS'
+      });
 
       return assert.eventually.equal(nativeBuilder.resolve(), electronSetup.concat(builder, ' rebuild --target=0.30.1', distUrl));
     });
@@ -61,18 +107,28 @@ describe('native-builder', function () {
         warnOnReplace: false,
         warnOnUnregistered: false
       });
+      this.env = Object.getOwnPropertyDescriptor(process, 'env');
+      this.env.BUILD_DEBUG = true;
+      Object.defineProperty(process, 'env', {
+        value: this.env
+      });
     });
 
     after(function () {
       mockery.deregisterMock('which-native-nodish');
       mockery.disable();
+
+      this.env.BUILD_DEBUG = false;
+      Object.defineProperty(process, 'env', {
+        value: this.env
+      });
     });
 
-    it('should work', function () {
+    it('should work in debug', function () {
       var nativeBuilder = require('../lib');
       var builder = path.resolve(__dirname, '..', 'node_modules', '.bin', 'nw-gyp');
 
-      return assert.eventually.equal(nativeBuilder.resolve(), builder.concat(' rebuild --target=0.12.0'));
+      return assert.eventually.equal(nativeBuilder.resolve(), builder.concat(' rebuild --target=0.12.0  --debug'));
     });
   });
 
