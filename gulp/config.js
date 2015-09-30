45510f4de2655943ba638a6c424148df01729a5a
Noam Okman <noamokman@gmail.com>
2015-09-30 13:41:12 +0300

    updated dependencies and removed old scripts tasks

--- gulp/config.js
+++ gulp/config.js
@@ -4,7 +4,6 @@ module.exports = {
   paths: {
     lib: './lib/**/*.js',
     test: './test/**/*.spec.js',
-    scripts: './scripts/**/*.js',
     gulp: ['./gulpfile.js', './gulp/**/*.js'],
     coverage: 'coverage/**/lcov.info',
     cliFile: './lib/cli.js'
